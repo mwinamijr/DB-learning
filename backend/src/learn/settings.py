506b8a24f9fb8299ba7448ce77fe8843426ef02a
mwinamijr <mwinamijr@gmail.com>
2020-02-07 16:58:31 +0300

    addede django-cors-headers to settings.py

--- backend/src/learn/settings.py
+++ backend/src/learn/settings.py
@@ -27,10 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'courses',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
