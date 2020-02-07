5db309e03204b8a020cf16f9d92f02a9967c9b2f
mwinamijr <mwinamijr@gmail.com>
2020-02-08 00:26:30 +0300

    modified app.js

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
+import { connect } from 'react-redux';
+import * as actions from './store/actions/auth';
 import { BrowserRouter as Router } from 'react-router-dom';
 import BaseRouter from './routes';
 
 import CustomLayout from './containers/Layouts';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,4 +26,19 @@ class App extends Component {
     );
   }
 }
-export default App;
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch (
+      actions.authCheckstate()
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
