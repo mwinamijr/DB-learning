3014ca00a160fc251057e1fb4fe8a99faae0badd
mwinamijr <mwinamijr@gmail.com>
2020-02-08 02:08:43 +0300

    added login and signup pages with it's contents

--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import CourseList from './containers/CourseListView';
 import CourseDetail from './containers/CourseDetailView';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 const BaseRouter = () => (
     <div>
         <Route exact path='/' component={CourseList} />
         <Route exact path='/:articleID' component={CourseDetail} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />        
     </div>
 );
 
