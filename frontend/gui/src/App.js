006dafcbbbcdeb1a092f54e2ae299e2fdcf78656
mwinamijr <mwinamijr@gmail.com>
2020-02-07 17:51:02 +0300

    modified routes and app.js

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
 
 import CustomLayout from './containers/Layouts';
 import CourseList from './containers/CourseListView';
 
-function App() {
-  return (
-    <div className="App">
-      <CustomLayout>
-        <CourseList />
-      </CustomLayout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <CustomLayout>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
+        
+      </div>
+    );
+  }
 }
-
 export default App;
--- frontend/gui/src/Routes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import CourseList from './containers/CourseListView';
-
-const BaseRouter = () => {
-    <Route exact path='/' component={CourseList} />
-    <Route exact path='/:articleID' component={CourseList} />
-}
