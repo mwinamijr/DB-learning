c6983a07dbb944bfe517d23d25489ad2f8182e5d
mwinamijr <mwinamijr@gmail.com>
2020-02-08 00:35:55 +0300

    modified by adding authCheckState in actios/auth.js

--- frontend/gui/src/store/actions/auth.js
+++ frontend/gui/src/store/actions/auth.js
@@ -80,3 +80,20 @@ export const authSignup = (username, password) => {
         })
     }
 }
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (token === undefined) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
+            } else {
+                dispatch(authSuccess(token));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000));
+            }
+        }
+    }
+}
